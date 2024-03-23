3a31790a7bc3239ffa51053da2163388119d62e4
Vamshi-nalla <nvkrishna9396@gmail.com>
2024-03-23 11:35:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Vamshi Nalla',
+    author_email= 'nvkrishna9396@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
